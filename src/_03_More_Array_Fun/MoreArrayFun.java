dafc3d1c37f696d8e9d050f6f1d03a453ad1e262
Michaelszeng <league@WTS14.attlocal.net>
2019-02-24 11:06:15 -0800

    ArrayTest, FireWork, MoreArrayFun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -3,24 +3,66 @@ package _03_More_Array_Fun;
 import java.util.Random;
 
 public class MoreArrayFun {
-	//1. Create a main method to test the other methods you write.
-	
-	
-	
-	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
-	
-	
-	//3. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in reverse order.
-	
-	
-	
-	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
-	
-	//5. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in a completely random order. Almost every run of the program should result in a different order.
-	
-	
+	// 1. Create a main method to test the other methods you write.
+	public static void main(String[] args) {
+		String[] array = { "yes", "hi", "word", "thats enough words" };
+
+		printRandomly(array);
+	}
+
+	// 2. Write a method that takes an array of Strings and prints all the Strings
+	// in the array.
+	public static void printStrings(String[] strings) {
+		for (String s : strings) {
+			System.out.println(s);
+		}
+	}
+
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in reverse order.
+	public static void reversePrintStrings(String[] strings) {
+		for (int i = strings.length - 1; i >= 0; i--) {
+			System.out.println(strings[i]);
+		}
+	}
+
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+	public static void printEveryOther(String[] strings) {
+		for (int i = 0; i < strings.length; i = i + 2) {
+			System.out.println(strings[i]);
+		}
+	}
+
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in a completely random order. Almost every run of the program should result
+	// in a different order.
+	public static void printRandomly(String[] strings) {
+		Boolean[] printed = new Boolean[strings.length];
+		for (int i = 0; i < printed.length; i++) {
+			printed[i] = false;
+		}
+		int checker = 4;
+		while (checker < 5) {
+			boolean printedAll = true;
+			Random ran = new Random();
+			int ranInt = ran.nextInt(strings.length);
+			if (printed[ranInt] == false) {
+				System.out.println(strings[ranInt]);
+				printed[ranInt] = true;
+			}
+			for (int i = 0; i < printed.length; i++) {
+				if (printed[i] == false) {
+					printedAll = false;
+				}
+			}
+			if (printedAll == true) {
+				checker = 6;
+			}
+
+		}
+	}
+
 }
